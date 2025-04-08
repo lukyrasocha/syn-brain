83bfb1dac1bbc88d55ff1970cee3bf753d4addda
Petr B Nylander <32863524+Cerlog@users.noreply.github.com>
2025-04-08 14:58:59 +0200

    Update data.py

--- src/data.py
+++ src/data.py
@@ -26,7 +26,8 @@ def download_extract_dataset(url, raw_data_path):
 
     
 if __name__ == "__main__":
-    DATASET_URL = "https://drive.google.com/uc?id=1cpjv0UYWgk9kRSGeBFxdK9HxCekGMDv-"
+    #DATASET_URL = "https://drive.google.com/uc?id=1cpjv0UYWgk9kRSGeBFxdK9HxCekGMDv-" # dataset divided into classes
+    DATASET_ULR = "https://drive.google.com/file/d/1eLOdNZ2--gAzWKtL3nV4FdVQw59XEEdh"
     RAW_DATA_PATH = Path("./data/raw")
     
     download_extract_dataset(DATASET_URL, RAW_DATA_PATH)
