c75cbdc4edafce2f36edb6058672f38b43bc58ac
MadsPrip1 <madsprip235@gmail.com>
2025-04-11 11:08:28 +0200

    udpate sd_lora

--- bash/sd_lora.sh
+++ bash/sd_lora.sh
@@ -2,7 +2,7 @@
 
 #BSUB -J sd_lora
 #BSUB -q gpuv100
-#BSUB -W 00:15
+#BSUB -W 00:05
 #BSUB -R "rusage[mem=10GB]"
 #BSUB -gpu "num=1"
 #BSUB -n 4
@@ -13,6 +13,7 @@
 # initialize conda in the script
 source activate base
 conda activate brain
+pip install bitsandbytes
 
 # The following shows how to use the accelerate denpending on the hardware  https://github.com/huggingface/accelerate/tree/main/examples
 python src/train_text_to_image_lora.py \
@@ -20,9 +21,12 @@ python src/train_text_to_image_lora.py \
     --dataset_name 'lambdalabs/naruto-blip-captions' \
     --output_dir "models/sd-naruto-model-lora" \
     --num_train_epochs 1 \
+    --train_batch_size 1 \
+    # --use_8bit_adam \
+    # --enable_xformers_memory_efficient_attention
+    # --mixed_precision="fp16" \
     # --variant 'fp16' \
     # --resolution 512 \
-    # --train_batch_size 16 \
     # --learning_rate 1e-4 \
     # --lr_scheduler 'constant' \
     # --lr_warmup_steps 500 \
