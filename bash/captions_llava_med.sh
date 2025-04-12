524853bd53992017a86a8310d80df39f68f18253
MadsPrip1 <madsprip235@gmail.com>
2025-04-12 19:47:05 +0200

    updated llava

--- bash/captions_llava_med.sh
+++ bash/captions_llava_med.sh
@@ -10,8 +10,10 @@
 #BSUB -o bash/bash_outputs/image_captioner_llava_med%J.out
 #BSUB -e bash/bash_outputs/image_captioner_llava_med%J.err
 
+# conda activate brain3
+
 source ~/.bashrc
-conda activate brain3
+conda activate brain
 
 #python src/describe_image.py --prompt "Describe what you see in this MRI image. Is there a tumor? If yes, describe the tumor's size, location, intensity and shape. Also describe the image orientation (axial, sagittal, or coronal) and any important visual features of the MRI." --image data/raw/Train_All_Images/meningioma_0549.jpg
 python src/image_captioner_llava_med.py --all 
