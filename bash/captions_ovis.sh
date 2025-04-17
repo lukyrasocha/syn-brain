68f0af96de64f856c71e8f1129c8a2a72f347138
lukyrasocha <rasocha.lukas@gmail.com>
2025-04-18 01:56:30 +0200

    update script

--- bash/captions_ovis.sh
+++ bash/captions_ovis.sh
@@ -2,17 +2,20 @@
 
 #BSUB -J image_captioner_ovis
 #BSUB -q gpua100
-#BSUB -W 12:00
-#BSUB -R "rusage[mem=16GB]"
+#BSUB -W 24:00
+#BSUB -R "rusage[mem=10GB]"
 #BSUB -R "select[gpu80gb]"
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 4
+##BSUB -u s233498@dtu.dk
+#BSUB -B
+#BSUB -N
 #BSUB -R "span[hosts=1]"
 #BSUB -o bash/bash_outputs/image_captioner_ovis%J.out
 #BSUB -e bash/bash_outputs/image_captioner_ovis%J.err
 
 source ~/.bashrc
-conda activate brain
+conda activate brain3
 
 #python src/describe_image.py --prompt "Describe what you see in this MRI image. Is there a tumor? If yes, describe the tumor's size, location, intensity and shape. Also describe the image orientation (axial, sagittal, or coronal) and any important visual features of the MRI." --image data/raw/Train_All_Images/meningioma_0549.jpg
 python src/image_captioner_ovis.py --all 
