9da623f76662e083d09b90b8ac4632d1e78ec970
lukyrasocha <rasocha.lukas@gmail.com>
2025-04-18 03:04:14 +0200

    update llava script with new env

--- bash/captions_llava_med.sh
+++ bash/captions_llava_med.sh
@@ -6,14 +6,14 @@
 #BSUB -R "rusage[mem=8GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 4
+##BSUB -u s233498@dtu.dk
+#BSUB -B
+#BSUB -N
 #BSUB -R "span[hosts=1]"
 #BSUB -o bash/bash_outputs/image_captioner_llava_med%J.out
 #BSUB -e bash/bash_outputs/image_captioner_llava_med%J.err
 
-# conda activate brain3
-
 source ~/.bashrc
-conda activate brain
+conda activate llava-med
 
-#python src/describe_image.py --prompt "Describe what you see in this MRI image. Is there a tumor? If yes, describe the tumor's size, location, intensity and shape. Also describe the image orientation (axial, sagittal, or coronal) and any important visual features of the MRI." --image data/raw/Train_All_Images/meningioma_0549.jpg
 python src/image_captioner_llava_med.py --all 
