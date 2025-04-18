fdaf3881b7e4962a8ff60c29b399baf821e3d035
lukyrasocha <rasocha.lukas@gmail.com>
2025-04-18 02:02:40 +0200

    cleanup

--- bash/captions_ovis.sh
+++ bash/captions_ovis.sh
@@ -17,5 +17,4 @@
 source ~/.bashrc
 conda activate brain3
 
-#python src/describe_image.py --prompt "Describe what you see in this MRI image. Is there a tumor? If yes, describe the tumor's size, location, intensity and shape. Also describe the image orientation (axial, sagittal, or coronal) and any important visual features of the MRI." --image data/raw/Train_All_Images/meningioma_0549.jpg
 python src/image_captioner_ovis.py --all
